edf2d67ee64b1185eb1d1ce680370e1ffe24002f
Vernon Grant <vernon@ruppell.io>
2022-06-17 19:52:08 +0800

    Sets up initial file template

---
+++ sidekick.el
@@ -0,0 +1,36 @@
+;;; sidekick.el --- A coding assistance panel  -*- lexical-binding: t -*-
+
+;; Copyright (C) 2009-2022  Free Software Foundation, Inc.
+
+;; Author: Vernon Grant <vernon@ruppell.io>
+;; Version: 0.0.0
+;; Package-Requires: ((emacs "24"))
+;; Keywords: extensions, lisp
+;; Homepage: https://github.com/VernonGrant/sidekick
+
+;; This program is free software: you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+;;; Commentary:
+
+;; A coding assistance panel.
+
+;;; Code:
+
+(defun sidekick-testing()
+  (interactive)
+  (print "Sidekick is kicking 2!"))
+
+(provide 'sidekick)
+
+;;; sidekick.el ends here
